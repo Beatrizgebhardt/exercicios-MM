7d4a4b8b259b7eabf8100983ada9d3423074f875
Beatriz Gebhardt <beatrizgra@outlook.com>
2022-09-05 22:33:15 -0300

    corrections

--- variaveis/exercicios.js
+++ variaveis/exercicios.js
@@ -7,8 +7,6 @@ const birthCity = "Goiânia";
 //Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
 let birthYear = 1998;
 birthYear = 2030;
-const = birthYear = 2050;
-console.log(birthYear);
 
 //Utilize o console.log() para imprimir as constantes e variáveis que você criou.
 console.log(myName);
@@ -16,7 +14,9 @@ console.log(birthCity);
 console.log(birthYear);
 
 //Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
-// linha 9
+birthYear = 2030;
+console.log(birthYear);
 
 //Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
 // a mensagem de erro ocorre pois uma constante não pode ser reatribuída.
+birthCity = "Londres";
