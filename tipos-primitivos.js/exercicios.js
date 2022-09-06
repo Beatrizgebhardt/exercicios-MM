e8cf8a9766ca6b8ea8eb6ef59dd8844de01ca7ce
Beatriz Gebhardt <beatrizgra@outlook.com>
2022-09-05 23:15:44 -0300

    tipos primitivos, tipagem dinâmica e operações aritméticas

---
+++ tipos-primitivos.js/exercicios.js
@@ -0,0 +1,29 @@
+//Utilize o operador typeof para imprimir qual o tipo das variáveis patientId, isEnrolled, patientInfo e patientEmail.
+// Esse operador retorna qual o tipo de uma variável, objeto, função ou expressão. Exemplo: console.log(typeof patientId) retornará number.
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: "Ana",
+  lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
+
+console.log(typeof patientId); //number
+console.log(typeof isEnrolled); // boolean
+console.log(typeof patientInfo); // object
+console.log(typeof patientEmail); //string
+
+console.log(typeof patientAge); // undefined
+
+//Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+const base = 5;
+let height = 8;
+
+//Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura.
+// Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+const area = base + height;
+console.log(area);
+
+//Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const perimeter = base + height + area;
+console.log(perimeter);
---
+++ tipos-primitivos.js/script.js
@@ -0,0 +1,26 @@
+let movie = "Avengers"; //strings literal
+let score = 10; //number literal
+let isValid = true; // boolean
+let name; //undefined
+let color = null; //nula, redefinição de valores
+
+/////////////////////////////////////////////////////////////////
+
+//+, -, *, /, **
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(3 ** 3); //3³
+
+/////////////////////////////////////////////////////////////////
+
+// incrementi e decremento
+
+salary++; //incrementa 1 valor:3501
+salary--; // decrementa 1 valor: 3499
+
+console.log(salary);
