d738f0149e4600a15a24c86a10acd1aa3b611904
Beatriz Gebhardt <beatrizgra@outlook.com>
2022-09-06 05:28:05 -0300

    corrections 2

---
+++ if-else.js/script.js
@@ -0,0 +1,26 @@
+// if - se
+// else - senão
+
+//if (condicao) {
+//codigo cao a condição seja verdadeira
+//} else if (outraCondicao) {
+//outra condicao
+//} else if (outraCondicao) {
+//outra condicao
+//} else {
+//se nenhuma condicao for verdadeira o else acontece
+//}
+
+////////////////////////////////////////////////////////
+
+let sabado = 14;
+
+if (sabado >= 14 && sabado < 14.4) {
+  console.log("esquenta");
+} else if (sabado >= 16.3 && sabado < 17.5) {
+  console.log("ao vivo");
+} else if (sabado >= 19.4 && sabado < 20) {
+  console.log("Fechamento");
+} else {
+  console.log("fora do sabado");
+}
